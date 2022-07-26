3a2e34709a8e7e73765d288ded14f45b1cb58443
DariaIS <dariastarodubova58@yandex.ru>
2022-07-26 13:18:22 +0300

    did pagination

--- pages/beers/index.tsx
+++ pages/beers/index.tsx
@@ -1,15 +1,19 @@
-import { FC } from "react";
-import { GetStaticProps } from "next";
+import { FC, useEffect } from "react";
+
+import { NextRouter, useRouter } from 'next/router'
 import Link from "next/link";
+import { GetServerSideProps } from "next";
+
 import { beerType } from "@types";
 
 type beersTypeProps = {
     beers: [beerType];
+    page: number;
 }
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getServerSideProps: GetServerSideProps = async ({ query: { page = 1 } }) => {
     const data =
-        await fetch('https://api.punkapi.com/v2/beers')
+        await fetch(`https://api.punkapi.com/v2/beers?page=${page}`)
             .then((response) => {
                 return response.json();
             });
@@ -21,20 +25,39 @@ export const getStaticProps: GetStaticProps = async () => {
     }
 
     return {
-        props: { beers: data },
+        props: {
+            beers: data,
+            page: +page,
+        },
     }
 };
 
-const Beers: FC<beersTypeProps> = ({ beers }) => (
-    <>
-        <ul>
-            {beers && beers.map(({ id, name }) => (
-                <li key={id}>
-                    <Link href={`${id}`}>{name}</Link>
-                </li>
-            ))}
-        </ul>
-    </>
-);
+const Beers: FC<beersTypeProps> = ({ beers, page }) => {
+    const router = useRouter();
+
+    return (
+        <>
+            <ul>
+                {beers && beers.map(({ id, name }) => (
+                    <li key={id}>
+                        <Link href={`/beers/${id}`}>{name}</Link>
+                    </li>
+                ))}
+            </ul>
+            <button
+                onClick={() => router.push(`/beers?page=${page - 1}`)}
+                disabled={page <= 1}
+            >
+                PREV
+            </button>
+            <button onClick={() => router.push(`/beers?page=${page + 1}`)}>
+                NEXT
+            </button>
+            <Link href="/?page=1">
+                <a>First page</a>
+            </Link>
+        </>
+    )
+};
 
 export default Beers;
