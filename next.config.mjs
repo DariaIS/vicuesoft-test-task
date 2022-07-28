5f7e2412fd9591c34cbd1f0daaffbf81f64709e2
DariaIS <93678283+DariaIS@users.noreply.github.com>
2022-07-28 18:11:16 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,9 @@ const nextConfig = {
         loader: 'akamai',
         path: '',
     },
+    env: {
+        NEXT_PUBLIC_BASE_PATH: 'my-value',
+    },
     basePath: process.env.NEXT_PUBLIC_BASE_PATH,
     assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
 };
